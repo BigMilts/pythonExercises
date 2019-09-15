4207592c62ad2bd3f65e0f8d278a8a7d96b6d008
Milton Souto Maior <51926931+BigMilts@users.noreply.github.com>
2019-09-15 18:31:36 -0300

    Update compreenssao de listas.py

--- listas/compreenssao de listas.py
+++ listas/compreenssao de listas.py
@@ -1,6 +1,6 @@
 #copreensao de listas
 #[z for z in range(alguma condição)if alguma condicao]
-
+#[y for y in lista if alguma condicao]
 #exemplo 
 lista1 = [1,2,3,4,5,6,7,8,9]
 pares = [x for x in lista1 if x %2==0]
