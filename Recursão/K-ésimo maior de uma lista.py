485eb4a322d51ba6f2d730ea1d21ebcdf3bb5e00
Milton Souto Maior <51926931+BigMilts@users.noreply.github.com>
2019-09-14 22:23:58 -0300

    Add files via upload

--- Recursão/K-ésimo maior de uma lista.py
+++ Recursão/K-ésimo maior de uma lista.py
@@ -1,29 +1,25 @@
-def maxR(lista):
+def maxr(lista):
     if len(lista) == 1:
         return lista[0]
     else:
         if lista[0]>lista[1]:
             lista.pop(1)
-            return maxR(lista)
+            return maxr(lista)
         else:
             lista.pop(0)
-            return maxR(lista)
+            return maxr(lista)
+print(maxr([14,3,5,3,33,99,33,108,22,1,3,4]))
 
-
-
-print(maxR([14,3,5,3,33,99,33,108,22,1,3,4]))
-
-def kMaior(lista,k):
+def k_maior(lista,k):
     aux = lista[:]
     if k ==1:
-        return maxR(aux)
+        return maxr(aux)
     else:
-        remover = maxR(aux)
+        remover = maxr(aux)
         lista.remove(remover)
-        return kMaior(lista,k-1)
+        return k_maior(lista,k-1)
 
-
-print(kMaior([14,3,5,3,33,99,33,108,22,1,3,4],3))
+print(k_maior([14,3,5,3,33,99,33,108,22,1,3,4],3))
 
 
 
