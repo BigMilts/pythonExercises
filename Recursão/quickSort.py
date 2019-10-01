be38c361feef8116e6b0da307a0d42037cf1ecad
Milton Souto Maior <51926931+BigMilts@users.noreply.github.com>
2019-09-30 22:16:40 -0300

    Update quickSort.py

--- Recursão/quickSort.py
+++ Recursão/quickSort.py
@@ -3,7 +3,7 @@ def quick_sort(lista):
         return []
     else:
         pivo=lista.pop(0)
-        return quick_sort([x for x in lista if x<=pivo]) + [pivo] +quick_sort([y for y in lista if y>pivo])    
+        return quick_sort([x for x in lista if x<=pivo]) + [pivo] + quick_sort([y for y in lista if y>pivo])    
 
 array=[6,768,6446,10,13,1,213123,-4,56,21]
 print(quick_sort(array))
