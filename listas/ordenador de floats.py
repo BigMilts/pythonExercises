cda2d7aacc9d570d0a35355a97c308704dc0d4a5
Milton Souto Maior <51926931+BigMilts@users.noreply.github.com>
2019-10-07 16:11:48 -0300

    Update ordenador de floats.py

--- listas/ordenador de floats.py
+++ listas/ordenador de floats.py
@@ -1,18 +1,10 @@
 #bublbe_sort
 vetor=[2.00,3.4,6.67,3.98,21.32,3.00]
 def ordenador(vetor):
-    elementos = 0
-    while elementos < len(vetor):
-        posicao = 0
-        while posicao < len(vetor)-1:
-            if vetor[posicao] > vetor[posicao+1]:
-                temp = vetor[posicao]
-                vetor[posicao] = vetor[posicao+1]
-                vetor[posicao+1] = temp
-                posicao+= 1
-            else:
-                posicao+= 1
-        elementos+= 1
-    return(vetor)
-
+   for i in range(1,len(vetor)-1):
+     for j in range(len(vetor)-1-i):
+            if vetor[j] >vetor[j+1]:troca(vetor,j,j+1)
+   return vetor
+def troca(A,i,j):
+    A[i],A[j] = A[j],A[i]
 print(ordenador(vetor))
